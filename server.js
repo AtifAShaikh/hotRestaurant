9e66ec0eab5280693ab52217e1a7c697baffc1cc
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-03-12 14:15:59 -0800

    heroku demo

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
